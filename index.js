cfe1ca816a2e7aaeeddc70a610db0d56127f8eec
Kyle E. Mitchell <kyle@kemitchell.com>
2018-01-18 14:12:02 -0800

    Handle HTTP errors

--- index.js
+++ index.js
@@ -16,9 +16,11 @@ limitations under the License.
 
 var concat = require('simple-concat')
 var https = require('https')
+var once = require('once')
 var parse = require('json-parse-errback')
 
 module.exports = function (repository, publisher, project, callback) {
+  callback = once(callback)
   https.request({
     host: repository,
     path: (
@@ -27,6 +29,8 @@ module.exports = function (repository, publisher, project, callback) {
       '/publications'
     )
   })
+    .once('error', callback)
+    .once('timeout', callback)
     .once('response', function (response) {
       var statusCode = response.statusCode
       if (statusCode === 404) return callback(null, false)
